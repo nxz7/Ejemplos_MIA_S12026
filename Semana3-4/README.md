4b161300f1510974a8fa3f124673ff1bae81818d
natalia <natalia7cald@gmail.com>
2026-02-11 21:46:48 -0600

    feat(clase3-4): mount y fdisk

---
+++ Semana3-4/README.md
@@ -0,0 +1,28 @@
+# Clase 3 y 4 - fdisk y mount
+
+Se usa la base de la Clase 2 y se agregan los comandos:
+
+
+- **fdisk**: crea particiones p/e/l
+    STRUCT: MBR, EBR, LIBRE, POSITION, LIBREL
+
+- **mount**: monta una partición y le asigna un id interno.
+
+
+## test
+
+```bash
+cd Semana3
+g++ -std=c++17 main.cpp analizador.cpp mkdisk.cpp rmdisk.cpp fdisk.cpp mount.cpp -o clase3
+./clase3
+```
+
+
+```bash
+mkdisk >size=5 >unit=m >fit=bf >path="/home/natalia/Documents/discos/Disco1.dsk"
+fdisk >size=1024 >unit=k >path="/home/natalia/Documents/discos/Disco1.dsk" >name=Part1 >type=p >fit=bf
+fdisk >size=1 >unit=m >path="/home/natalia/Documents/discos/Disco1.dsk" >name=PartEXT2 >type=e >fit=ff
+fdisk >size=100 >unit=k >path="/home/natalia/Documents/discos/Disco1.dsk" >name=Partlogica >type=l >fit=wf
+mount >path="/home/natalia/Documents/discos/Disco1.dsk" >name=Partlogica
+
+```
---
+++ Semana3-4/structs.h
@@ -0,0 +1,180 @@
+#ifndef STRUCTS
+#define STRUCTS
+
+#include <time.h>
+#include <string>
+#include <vector>
+#include <regex>
+
+// 0: inactiva, 1: activa
+
+// PARTICION EN EL MBR - DATA A ALMACENAR y luego mostrar en el reporte
+struct particion {
+    char part_status;      
+    char part_type;        // P: primaria, E: extendida, L: logica
+    char part_fit;         // best, worst, first
+    int part_start;        // Inicio en bytes
+    int part_s;            // SIZE
+    char part_name[16];    // Nombre
+};
+
+// MBR DEL DISCO
+struct MBR {
+    int mbr_tamano;
+    time_t mbr_fecha_creacion;
+    int mbr_dsk_signature;
+    char dsk_fit;
+    particion mbr_partition[4];
+};
+
+//----------------------------------------------------
+
+//********** EBR **********
+struct EBR{
+    char part_status;                       //0/1
+    char part_fit;                    // best, worst, first
+    int part_start;                         //Posicion inicial
+    int part_s;                             //tamaño
+    int part_next;                          //sigueinte
+    char part_name[16];                     //Nombre de la partición logica
+
+};
+
+//********** POSICIONES DE LAS PARTICIONES **********
+//POSICIONES DE LAS PARTICIONES
+
+struct position{
+    // inicio = en donde empieza la particion
+    // fin = en donde termina la particion
+    
+    int inicio;                             
+    int fin;
+    char tipo;
+    std::string nombre;
+    int size; 
+
+    bool operator<(const position& a) const
+    {
+        return inicio < a.inicio;
+    }
+};
+
+// --------- ESPACIOS LIBRES particiones primarias y extendida
+struct libre{
+    int inicio;
+    int size;
+
+    bool operator<(const libre& a) const
+    {
+        return size < a.size;
+    }
+};
+
+//espacio vacio - PARTICIONES LOGICAS
+struct libreL{
+    int inicioEBR;              //leerl el inicio
+    int finLogica;              //Donde termina la particion logica
+    int size;                 //Espacio Libre
+    bool header;              //si es la inicial
+
+    bool operator<(const libreL& a) const
+    {
+        return size < a.size;
+    }
+};
+
+
+
+//-----------------------------MOUNT
+
+
+//---------- MONTAR DISCO
+
+struct montada{
+    std::string id;                             //ID de a particion montada
+    int posEBR = -1;                            //Si es logica es diferente a -1
+    int posMBR = -1;                            //Si no es logica es diferente a -1
+    std::string nombre;                         //Nombre de la particion
+    int size;                                 //size de la particion en bytes
+};
+
+//MANEJA LOS DATOS DE LAS PARTICIONES MONTADAS
+struct disco{
+    std::string ruta;                           //Ruta del disco
+    std::string nombre;                         //Nombre del disco
+    int contador = 1;                           //Sirve para el ID de la montada
+    std::vector<montada> particiones;           //Particiones del disco montadas
+};
+
+//SUPER
+struct sbloque{
+    int s_filesystem_type;              //EXT2
+    int s_inodes_count;                 
+    int s_blocks_count;                 
+    int s_free_blocks_count;            
+    int s_free_inodes_count;            
+    time_t s_mtime;                     
+    time_t s_umtime;                    
+    int s_mnt_count;                    
+    int s_magic;                        
+    int s_inode_s;                      
+    int s_block_s;                      
+    int s_first_ino;                    
+    int s_first_blo;                    
+    int s_bm_inode_start;               
+    int s_bm_block_start;               
+    int s_inode_start;                  
+    int s_block_start;                  
+};
+
+//INODES
+struct inodo{
+    int i_uid;                                                    
+    int i_gid;                          
+    int i_s;                            
+    time_t i_atime;                     
+    time_t i_ctime;                     
+    time_t i_mtime;                     
+    int i_block[15];                    
+    char i_type;                        
+    int i_perm;                         
+};
+
+//BLOQUES
+struct content{
+    char b_name[12];
+    //numeor del inodo en el que esta el arhico/file                    
+    int b_inodo;                        
+};
+
+struct bcarpetas{
+    content b_content[4];                 
+};
+
+struct barchivos{
+    char b_content[64];         
+};
+
+struct bapuntadores{
+    //posicion del otrro bloqie
+    int b_pointers[16];                 
+};
+
+//registros, journaling - en caso de ext3
+
+//se usa para el login
+struct usuario{
+    std::string user;                   
+    std::string pass;                   
+    std::string disco;                  
+    std::string grupo;                  
+    std::string id_user;                
+    std::string id_grp;                 
+};
+
+// termina el mbr y ya se puede escribir la primera particion
+const int EndMBR = sizeof(MBR) + 1;     
+const std::regex igual("=");
+
+#endif
+
